382e106fef71b40237a512c2f1cd213b0f0f7c05
Kazuki Tsuda <wkt1227@gmail.com>
2020-12-13 13:53:04 +0900

    Updated funcs.py

--- src/libs/funcs.py
+++ src/libs/funcs.py
@@ -4,7 +4,10 @@ from pydl.photoop.photoobj import unwrap_objid
 import requests
 import xml.etree.ElementTree as ET
 import mahotas
-
+import re
+from bs4 import BeautifulSoup
+from urllib3.util import Retry
+from requests.adapters import HTTPAdapter
 
 
 # 画像からパワースペクトルを計算する
@@ -82,12 +85,72 @@ def get_zernikemoments_from_img(img, radius=10):
 
 def get_z_from_objid(objid):
     sqlCmd = f'SELECT * FROM SpecObjAll WHERE bestObjId={objid}'
-    response = requests.post(
+    # response = requests.post(
+    #     'http://skyserver.sdss.org/dr7/en/tools/search/x_sql.asp',
+    #     {
+    #         'format': 'xml',
+    #         'cmd': sqlCmd,
+    #     }
+    # )
+
+    # session作成
+    session = requests.Session()    
+    retries = Retry(total=5,  # リトライ回数
+                    backoff_factor=1,  # sleep時間
+                    status_forcelist=[500, 502, 503, 504])  # タイムアウト以外でリトライするステータスコード
+    session.mount('http://', HTTPAdapter(max_retries=retries))
+
+    # postリクエスト
+    response = session.post(url='http://skyserver.sdss.org/dr7/en/tools/search/x_sql.asp',
+                           data={
+                               'format': 'xml',
+                               'cmd': sqlCmd,
+                           },
+                           stream=True,
+                           timeout=(10.0, 30.0))
+    # responseをxmlとして読み込む
+    root = ET.fromstring(response.text)
+    try:
+        # zを探して、返す。
+        return float(root[1][0].attrib['z'])
+    except KeyError:  # bestObjIdとobjidが一致しない場合
+        print(objid)
+        return get_z_from_objid2(objid)
+
+
+# bestObjId != objidの場合、使用する。HTTPリクエスト2回。
+def get_z_from_objid2(objid):
+    # session作成
+    session = requests.Session()    
+    retries = Retry(total=5,  # リトライ回数
+                    backoff_factor=1,  # sleep時間
+                    status_forcelist=[500, 502, 503, 504])  # タイムアウト以外でリトライするステータスコード
+    session.mount('http://', HTTPAdapter(max_retries=retries))
+
+    # specObjIdを探す
+    url1 = f'http://skyserver.sdss.org/dr7/en/tools/explore/OETOC.asp?id={objid}'
+    response1 = session.get(url1, stream=True, timeout=(10.0, 30.0))
+    html1 = response1.text
+    # soup1 = BeautifulSoup(html1, 'html.parser')
+    # s1 = soup1.div.table.find_all('tr')[23].a
+    # m1 = re.search('href="(.*?)"', str(s1))
+    # s2 = m1[1]
+    # m2 = re.search('specObjId=(.*?)&', s2)
+    # specObjId = m2[1]
+    m = re.search('href="(.*?)"', html1)
+    specObjId = m[1]
+
+    # 取得したspecObjIdの行をSpecObjAllから取ってくる。
+    sqlCmd = f'SELECT * FROM SpecObjAll WHERE specObjId={specObjId}'
+    response2 = session.post(
         'http://skyserver.sdss.org/dr7/en/tools/search/x_sql.asp',
         {
             'format': 'xml',
-            'cmd': sqlCmd,
-        }
+            'cmd': sqlCmd
+        },
+        stream=True,
+        timeout=(10.0, 30.0)
     )
-    root = ET.fromstring(response.text)
-    return root[1][0].attrib['z'] 
+    root = ET.fromstring(response2.text)
+    # zを探して、返す。
+    return float(root[1][0].attrib['z'])
